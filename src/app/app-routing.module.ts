aa3005bbc257b927827b1dd6e29dd251f383716d
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-02-22 10:33:29 -0500

    added center component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CenterComponent} from './center/center.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CenterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
